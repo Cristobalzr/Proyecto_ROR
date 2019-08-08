99cf0f6f4e72eb1d7c32999bc30747fdff07bf5f
Cristobal <cristobalz@outlook.com>
2019-08-07 20:08:49 -0400

    Creo los modelos y las referencias

---
+++ db/migrate/20190808000546_create_matches.rb
@@ -0,0 +1,11 @@
+class CreateMatches < ActiveRecord::Migration[5.2]
+  def change
+    create_table :matches do |t|
+      t.references :user, foreign_key: true
+      t.references :pet, foreign_key: true
+      t.references :action, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
