e6e1af435680e1beca54e46c2b2fc572cbf07acb
Cristobal <cristobalz@outlook.com>
2019-08-07 21:39:30 -0400

    Creo admin y roles en cancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,12 +6,20 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
+      user ||= User.new # guest user (not logged in)
+      if user.admin?
+        can :manage, :all
+      elsif user.visit?
+        can :read, :all
+      elsif user.natural?
+        can :read, :all
+      elsif user.foundation?
+        can :read, :all
+      elsif user.veterinary?
+        can :read, :all
+      else
+        can :read, :all
+      end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_08_000546) do
+ActiveRecord::Schema.define(version: 2019_08_08_012714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 2019_08_08_000546) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "password"
     t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
